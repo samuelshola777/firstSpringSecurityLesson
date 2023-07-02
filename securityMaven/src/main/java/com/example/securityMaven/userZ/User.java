8239c032c6aeb6f1d9e8b25f1a83b1a8598a21cd
samuelshola777 <samuelshola14@gmail.com>
2023-07-02 19:14:30 +0100

    gol

--- securityMaven/src/main/java/com/example/securityMaven/userZ/User.java
+++ securityMaven/src/main/java/com/example/securityMaven/userZ/User.java
@@ -1,17 +1,16 @@
 package com.example.securityMaven.userZ;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -26,6 +25,7 @@ public class User implements UserDetails {
     @Id
   @GeneratedValue(strategy = IDENTITY)
     private Integer id;
+    @Enumerated(EnumType.STRING)
     private Role role;
     private String firstName;
     private String lastName;
@@ -34,7 +34,12 @@ public class User implements UserDetails {
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return null;
+    return List.of(new SimpleGrantedAuthority(role.name()));
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
   }
 
   @Override
@@ -44,21 +49,21 @@ public class User implements UserDetails {
 
   @Override
   public boolean isAccountNonExpired() {
-    return false;
+    return true;
   }
 
   @Override
   public boolean isAccountNonLocked() {
-    return false;
+    return true;
   }
 
   @Override
   public boolean isCredentialsNonExpired() {
-    return false;
+    return true;
   }
 
   @Override
   public boolean isEnabled() {
-    return false;
+    return true;
   }
 }
