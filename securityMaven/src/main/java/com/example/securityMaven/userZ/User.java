dfa6711163798d1f368abcdec785447214c7485e
samuelshola777 <samuelshola14@gmail.com>
2023-07-02 18:04:14 +0100

    gol

--- securityMaven/src/main/java/com/example/securityMaven/userZ/User.java
+++ securityMaven/src/main/java/com/example/securityMaven/userZ/User.java
@@ -24,7 +24,7 @@ public class User {
     private Integer id;
     private String firstName;
     private String lastName;
-    private String phone;
+
     private String email;
     private String password;
 }
