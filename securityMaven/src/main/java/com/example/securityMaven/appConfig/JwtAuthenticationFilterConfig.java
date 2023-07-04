ebeb7e8c43c4acd2b6cd39b37a86c16c058b6d4c
samuelshola777 <samuelshola14@gmail.com>
2023-07-04 20:00:47 +0100

    read token

--- securityMaven/src/main/java/com/example/securityMaven/appConfig/JwtAuthenticationFilterConfig.java
+++ securityMaven/src/main/java/com/example/securityMaven/appConfig/JwtAuthenticationFilterConfig.java
@@ -6,6 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.context.SecurityContextHolderFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,6 +21,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilterConfig extends OncePerRequestFilter {
     @NonNull
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         final String authHeader= request.getHeader("Authorization");
@@ -27,5 +33,13 @@ public class JwtAuthenticationFilterConfig extends OncePerRequestFilter {
         }
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUserName(jwt);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+       if (jwtService.isTokenValid(jwt, userDetails)){
+           UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                   userDetails,null, userDetails.getAuthorities();
+           )
+       }
+        }
     }
 }
--- securityMaven/src/main/java/com/example/securityMaven/appConfig/JwtService.java
+++ securityMaven/src/main/java/com/example/securityMaven/appConfig/JwtService.java
@@ -2,23 +2,56 @@ package com.example.securityMaven.appConfig;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
     @Value("${SecurityMavenApplicationSecretKey}")
     private  String SECRET_KEY ;
-    public String extractUserName(String jwt) {
-return null;
+    public String extractUserName(String token) {
+return extractClaim(token, Claims::getSubject);
+    }
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(),userDetails);
+    }
+    public boolean isTokenValid(String token, UserDetails userDetails ){
+        final String username = extractUserName(token);
+        return (username.equals(userDetails.getUsername()))&& !isTokenExpired(token);
     }
 
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
 
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
 
+    public String generateToken(Map<String, Object> extractClaims, UserDetails userDetails) {
+        return  Jwts.builder()
+                .setClaims(extractClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+}
+
+public <T> T extractClaim(String token , Function<Claims, T> claimResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+}
 
 
     private Claims extractAllClaims(String token){
