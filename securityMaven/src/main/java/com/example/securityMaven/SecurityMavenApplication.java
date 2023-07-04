6af31a81e12f6b2bd46f241e6444912afbf4fca1
samuelshola777 <samuelshola14@gmail.com>
2023-07-04 13:15:13 +0100

    read token

--- securityMaven/src/main/java/com/example/securityMaven/SecurityMavenApplication.java
+++ securityMaven/src/main/java/com/example/securityMaven/SecurityMavenApplication.java
@@ -1,5 +1,7 @@
 package com.example.securityMaven;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,14 @@ public class SecurityMavenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityMavenApplication.class, args);
+	}
+	private Claims extractAllClaims(String token){
+		return Jwts
+				.parserBuilder()
+				.setSigningKey(getSigningKey())
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
 	}
 
 }
