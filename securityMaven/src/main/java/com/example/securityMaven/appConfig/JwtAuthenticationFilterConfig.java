ccfac9b944728f2143261e7801fe2b4193124cec
samuelshola777 <samuelshola14@gmail.com>
2023-07-04 12:08:16 +0100

    add dependency

--- securityMaven/src/main/java/com/example/securityMaven/appConfig/JwtAuthenticationFilterConfig.java
+++ securityMaven/src/main/java/com/example/securityMaven/appConfig/JwtAuthenticationFilterConfig.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -13,14 +14,18 @@ import java.io.IOException;
 @RequiredArgsConstructor
 
 public class JwtAuthenticationFilterConfig extends OncePerRequestFilter {
+    @NonNull
+    private final JwtService jwtService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         final String authHeader= request.getHeader("Authorization");
         final String jwt;
+        final String userEmail;
         if (authHeader != null ||! authHeader.startsWith("Bearer ")) {
          filterChain.doFilter(request, response);
          return;
         }
         jwt = authHeader.substring(7);
+        userEmail =
     }
 }
---
+++ securityMaven/src/main/java/com/example/securityMaven/appConfig/JwtService.java
@@ -0,0 +1,7 @@
+package com.example.securityMaven.appConfig;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+}
