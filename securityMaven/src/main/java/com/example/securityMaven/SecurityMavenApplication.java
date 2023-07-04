58b3c69b857eedae883d170ba7058f0fece52e2f
samuelshola777 <samuelshola14@gmail.com>
2023-07-04 15:41:34 +0100

    read token

--- securityMaven/src/main/java/com/example/securityMaven/SecurityMavenApplication.java
+++ securityMaven/src/main/java/com/example/securityMaven/SecurityMavenApplication.java
@@ -12,24 +12,10 @@ import java.security.Key;
 
 @SpringBootApplication
 public class SecurityMavenApplication {
-@Value("${SecurityMavenApplicationSecretKey}")
-	private  String SECRET_KEY ;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityMavenApplication.class, args);
 	}
-	private Claims extractAllClaims(String token){
-		return Jwts
-				.parserBuilder()
-				.setSigningKey(getSigningKey())
-				.build()
-				.parseClaimsJws(token)
-				.getBody();
-	}
 
-	private Key getSigningKey() {
-		byte [] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
-
-		return Keys.hmacShaKeyFor(keyBytes);
-	}
 
 }
--- securityMaven/src/main/java/com/example/securityMaven/appConfig/JwtService.java
+++ securityMaven/src/main/java/com/example/securityMaven/appConfig/JwtService.java
@@ -1,10 +1,38 @@
 package com.example.securityMaven.appConfig;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+    @Value("${SecurityMavenApplicationSecretKey}")
+    private  String SECRET_KEY ;
     public String extractUserName(String jwt) {
+return null;
+    }
 
+
+
+
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte [] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
