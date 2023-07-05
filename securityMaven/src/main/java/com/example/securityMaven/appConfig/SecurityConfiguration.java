3a8e578489f924d72c63b8ce42a79713436823da
samuelshola777 <samuelshola14@gmail.com>
2023-07-05 06:05:50 +0100

    read token

--- securityMaven/src/main/java/com/example/securityMaven/appConfig/SecurityConfiguration.java
+++ securityMaven/src/main/java/com/example/securityMaven/appConfig/SecurityConfiguration.java
@@ -1,14 +1,21 @@
 package com.example.securityMaven.appConfig;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+
+    }
 
 
 
