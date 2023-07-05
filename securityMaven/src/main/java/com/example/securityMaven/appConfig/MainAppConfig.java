f4a638c992b5625f2239032a7c032c0484b2940e
samuelshola777 <samuelshola14@gmail.com>
2023-07-05 14:53:19 +0100

    update

--- securityMaven/src/main/java/com/example/securityMaven/appConfig/MainAppConfig.java
+++ securityMaven/src/main/java/com/example/securityMaven/appConfig/MainAppConfig.java
@@ -5,6 +5,8 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -23,4 +25,8 @@ public class MainAppConfig {
 
      }
 
+     @Bean
+    public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    }
 }
