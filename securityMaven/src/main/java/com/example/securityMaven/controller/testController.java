f05f316277f1bea8e59e82eed857b8eed2650d62
samuelshola777 <samuelshola14@gmail.com>
2023-07-05 12:25:45 +0100

    testing api

---
+++ securityMaven/src/main/java/com/example/securityMaven/controller/testController.java
@@ -0,0 +1,15 @@
+package com.example.securityMaven.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class testController {
+@GetMapping("/")
+    public String testController(){
+        return "my name is  samuel shola";
+    }
+
+}
